df386def9795f2d6384516860517e402f096cc7e
LesPrimus <amerigo.armentano@hotmail.it>
2021-03-11 19:59:49 +0100

    switch to postgres

--- Drf_jwt_otp_package/settings.py
+++ Drf_jwt_otp_package/settings.py
@@ -80,13 +80,17 @@ WSGI_APPLICATION = 'Drf_jwt_otp_package.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
